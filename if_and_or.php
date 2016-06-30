0ae712601237c3e235cae5dfa590a26a24c79306
Randi Mays <randimays1024@gmail.com>
2016-06-30 15:04:17 -0500

    Adding third section of If/Or lesson

--- if_and_or.php
+++ if_and_or.php
@@ -15,11 +15,19 @@ if ($x < $y && $y < $z) {
 // then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
 
 if (0 < $x || $x < 10) {
-	echo "0 is less than {$x} OR {$x} is less than 10.";
+	echo "0 is less than {$x} OR {$x} is less than 10\n";
 }
 
 // TODO:
 // repeat the if statement for $y and $z.
+
+if (0 < $y || $y < 10) {
+	echo "0 is less than {$y} OR {$y} is less than 10\n";
+}
+
+if (0 < $z || $z < 10) {
+	echo "0 is less than {$z} OR {$z} is less than 10\n";
+}
 
 // TODO:
 // If 0 is less than $x AND $x is less than 10
