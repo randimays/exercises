7caa40071a8d512649482f1f66a5c54f3e9a8baa
Randi Mays <randimays1024@gmail.com>
2016-07-05 10:47:33 -0500

    Updating highlow exercise

--- highlow.php
+++ highlow.php
@@ -1,29 +1,36 @@
 <?php
 
-if ($argc == 3) {
+$min = 1;
+$max = 3;
 
-	$random = mt_rand($argv[1], $argv[2]);
-	$guessCount = 0;
-
-	fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]}. Press ctrl + c to quit.\n");
-
-	do {
-		$guessCount++;
-		fwrite(STDOUT, "Guess? ");
-		$guess = trim(fgets(STDIN));
-		if ($guess > $random) {
-			fwrite(STDOUT, "Lower\n");
-		} elseif ($guess < $random) {
-			fwrite(STDOUT, "Higher\n");
-		} else {
-			fwrite(STDOUT, "Good Guess!\n");
-		}
-	} while ($guess != $random);
-
-	fwrite(STDOUT, "Total Guesses: $guessCount\n");
-
-	exit(0);
+if ($argc === 3 && is_numeric($argv[1]) && is_numeric($argv[2])) {
+	$min = $argv[1];
+	$max = $argv[2];
+} else {
+	fwrite(STDERR, "You need to pass 2 numeric values as well.\n");
+	exit(1);
 }
+
+$random = mt_rand($min, $max);
+$guessCount = 0;
+
+fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]}. Press ctrl + c to quit.\n");
+
+do {
+	$guessCount++;
+	fwrite(STDOUT, "Guess? ");
+	$guess = trim(fgets(STDIN));
+	if ($guess > $random) {
+		fwrite(STDOUT, "Lower\n");
+	} elseif ($guess < $random) {
+		fwrite(STDOUT, "Higher\n");
+	} else {
+		fwrite(STDOUT, "Good Guess! Total Guesses: $guessCount\n");
+		exit(0);
+	}
+} while ($guess != $random);
+
+
 
 
 
