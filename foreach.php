ceaa6ab0fcc4b19289d606269998526991350fa6
Randi Mays <randimays1024@gmail.com>
2016-07-05 15:26:11 -0500

    Updating first section of foreach exercise for PHP

--- foreach.php
+++ foreach.php
@@ -1,9 +1,15 @@
 <?php
 
-$things = array('Sgt. Pepper', "11", null, [1,2,3], 3.14, "12 + 7", false, (string) 11);
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		echo "{$thing}\n";
+	if (is_array($thing)) {
+		echo "Inner array:\n";
+		foreach ($thing as $number) {
+			echo "$number " . PHP_EOL;
+		}
+		echo "\n";
+	} else {P_EOL;
 	}
+	echo $thing.PH
 }
