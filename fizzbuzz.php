21c643406c25740673d2e89ea2937ad075c9e1c8
Randi Mays <randimays1024@gmail.com>
2016-07-05 12:30:45 -0500

    Adding fizzbuzz exercise

---
+++ fizzbuzz.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+	if ($i % 15 == 0) {
+		fwrite(STDOUT, "FizzBuzz\n");
+	} elseif ($i % 3 == 0) {
+		fwrite(STDOUT, "Fizz\n");
+	} elseif ($i % 5 == 0) {
+		fwrite(STDOUT, "Buzz\n");
+	} else {
+		fwrite(STDOUT, "$i\n");
+	}
+}
