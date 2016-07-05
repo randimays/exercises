2631a31a4f47de11a40d8f6e891acf22d4bfffd8
Randi Mays <randimays1024@gmail.com>
2016-07-05 11:57:59 -0500

    Adding second section of for loop exercise

--- for.php
+++ for.php
@@ -6,6 +6,13 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, "Enter an ending number: \n");
 $end = trim(fgets(STDIN));
 
-for ($i = $start; $i <= $end; $i++) {
-	fwrite(STDOUT, "$i\n");
+fwrite(STDOUT, "Choose an numeric increment: \n");
+$increment = trim(fgets(STDIN));
+
+if (is_numeric($increment)){
+	for ($i = $start; $i <= $end; $i += $increment) {
+		fwrite(STDOUT, "$i\n");
+	}
+} else {
+	fwrite(STDOUT, "You must enter a number for me to count by.");
 }
