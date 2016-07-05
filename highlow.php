1f2e39d7b495e6913474191d8cfd97744aa212d9
Randi Mays <randimays1024@gmail.com>
2016-07-05 10:28:43 -0500

    Updating highlow exercise for 6.2.2

--- highlow.php
+++ highlow.php
@@ -1,26 +1,29 @@
 <?php
 
-$random = mt_rand(1, 100);
-$guess;
-$guessCount = 0;
+if ($argc == 3) {
 
-fwrite(STDOUT, "Guess a number between 1 and 100.\n");
+	$random = mt_rand($argv[1], $argv[2]);
+	$guessCount = 0;
 
-do {
-	$guessCount++;
-	fwrite(STDOUT, "Guess? ");
-	$guess = trim(fgets(STDIN));
-	if ($guess > $random) {
-		fwrite(STDOUT, "Lower\n");
-	} elseif ($guess < $random) {
-		fwrite(STDOUT, "Higher\n");
-	} else {
-		fwrite(STDOUT, "Good Guess!\n");
-	}
-} while ($guess != $random);
+	fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]}. Press ctrl + c to quit.\n");
 
-fwrite(STDOUT, "Total Guesses: $guessCount\n");
-exit(0);
+	do {
+		$guessCount++;
+		fwrite(STDOUT, "Guess? ");
+		$guess = trim(fgets(STDIN));
+		if ($guess > $random) {
+			fwrite(STDOUT, "Lower\n");
+		} elseif ($guess < $random) {
+			fwrite(STDOUT, "Higher\n");
+		} else {
+			fwrite(STDOUT, "Good Guess!\n");
+		}
+	} while ($guess != $random);
+
+	fwrite(STDOUT, "Total Guesses: $guessCount\n");
+
+	exit(0);
+}
 
 
 
