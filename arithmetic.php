bac1eaf07c03c69027da6f0055975439bc497636
Randi Mays <randimays1024@gmail.com>
2016-07-07 11:16:45 -0500

    Adding new exercise for 6.4.1

--- arithmetic.php
+++ arithmetic.php
@@ -4,23 +4,43 @@ $a = 50;
 $b = 25;
 
 function add($a, $b) {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a + $b;
+	} else {
+		return "ERROR: Both arguments must be numbers.\n"
+	}
 }
 
 function subtract($a, $b) {
-    return $a - $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a - $b;
+	} else {
+		return "ERROR: Both arguments must be numbers.\n"
+	}
 }
 
 function multiply($a, $b) {
-    return $a * $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a * $b;
+	} else {
+		return "ERROR: Both arguments must be numbers.\n"
+	}
 }
 
 function divide($a, $b) {
-    return $a / $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a / $b;
+	} else {
+		return "ERROR: Both arguments must be numbers.\n"
+	}
 }
 
 function modulus($a, $b) {
-	return $a % $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+	} else {
+		return "ERROR: Both arguments must be numbers.\n"
+	}
 }
 
 echo add(3, 4) . PHP_EOL;
--- student_grades.php
+++ student_grades.php
@@ -1,49 +1,110 @@
 <?php
 
-$awesomeGrade = 80;
+$student = [
+	'awesomeGrade' => 80,
+	'name' => null,
+	'subjects' => [],
+];
 
-function askName ($message) {
-	fwrite(STDOUT, $message);
-	$studentName = trim(fgets(STDIN));
-	return $studentName;
-};
-
-function askSubject ($message) {
-	fwrite(STDOUT, $message);
-	$subject = trim(fgets(STDIN));
-	return $subject;
-};
-
-function collectGrades($message) {
-	$allGrades = [];
-	do {
-		fwrite(STDOUT, $message);
-		$grade = trim(fgets(STDIN));
-		array_push($allGrades, (int)$grade);
-		fwrite(STDOUT, "Do you have more grades? Type 'y' or 'n'.\n");
-		$moreGrades = trim(fgets(STDIN));
-	} while ($moreGrades == "y");
-	return $allGrades;
-};
-
-function calculateAverage($grades, $subject) {
-	$sumGrades = 0;
+// Student's methods
+function calculateAverage($subjects) {
 	$average = 0;
-	foreach ($grades as $grade) {
-		$sumGrades += (int)$grade;
+	foreach ($subjects as $subject) {
+		$average += $subject['grade'];
 	}
-	$average = $sumGrades / count($grades);
-	echo "Your " . $subject . " average is: " . number_format($average, 2) . ".\n";
-	return $average;
-};
-
-$studentName = askName("What's your name?\n");
-$subject = askSubject("What's the name of the subject?\n");
-$grades = collectGrades("Enter a grade:\n");
-$average = calculateAverage($grades, $subject);
-
-if ($average >= $awesomeGrade) {
-	fwrite(STDOUT, "{$studentName}, you're an awesome student!\n");
-} else {
-	fwrite(STDOUT, "{$studentName}, you should probably study more.\n");
+	return $average / count($subjects);
 }
+
+function addSubject(&$subjects, $name, $grade) {
+	$subject = [
+		'name' => $name,
+		'grade' => $grade
+	];
+	$subjects[] = $subject;
+}
+
+function isAwesome($subjects, $awesomeGrade) {
+	return calculateAverage($subjects) > $awesomeGrade;
+}
+
+// Console methods
+function prompt($message) {
+	alert($message);
+	return trim(fgets(STDIN));
+}
+
+function confirm($message) {
+	return strtolower(prompt($message)) === 'y';
+}
+
+function alert($message) {
+	fwrite(STDOUT, $message . PHP_EOL);
+}
+
+// Input
+$student['name'] = prompt("What's your name?");
+
+do {
+	$name = prompt("What's the name of the subject?");
+	$grade = prompt("What's your grade?");
+	addSubject($student['subjects'], $name, $grade);
+} while (confirm('Do you want to add another grade?'));
+
+// Process
+$average = number_format(calculateAverage($student['subjects']), 2);
+
+// Output
+if (isAwesome($student['subjects'], $student['awesomeGrade'])) {
+	alert($student['name'] . " you're Awesome!!!! Your average was " . $average);
+} else {
+	alert($student['name'] . " you need more practice. Your average was " . $average);
+}
+
+// My original solution
+// $awesomeGrade = 80;
+
+// function askName ($message) {
+// 	fwrite(STDOUT, $message);
+// 	$studentName = trim(fgets(STDIN));
+// 	return $studentName;
+// };
+
+// function askSubject ($message) {
+// 	fwrite(STDOUT, $message);
+// 	$subject = trim(fgets(STDIN));
+// 	return $subject;
+// };
+
+// function collectGrades($message) {
+// 	$allGrades = [];
+// 	do {
+// 		fwrite(STDOUT, $message);
+// 		$grade = trim(fgets(STDIN));
+// 		array_push($allGrades, (int)$grade);
+// 		fwrite(STDOUT, "Do you have more grades? Type 'y' or 'n'.\n");
+// 		$moreGrades = trim(fgets(STDIN));
+// 	} while ($moreGrades == "y");
+// 	return $allGrades;
+// };
+
+// function calculateAverage($grades, $subject) {
+// 	$sumGrades = 0;
+// 	$average = 0;
+// 	foreach ($grades as $grade) {
+// 		$sumGrades += (int)$grade;
+// 	}
+// 	$average = $sumGrades / count($grades);
+// 	echo "Your " . $subject . " average is: " . number_format($average, 2) . ".\n";
+// 	return $average;
+// };
+
+// $studentName = askName("What's your name?\n");
+// $subject = askSubject("What's the name of the subject?\n");
+// $grades = collectGrades("Enter a grade:\n");
+// $average = calculateAverage($grades, $subject);
+
+// if ($average >= $awesomeGrade) {
+// 	fwrite(STDOUT, "{$studentName}, you're an awesome student!\n");
+// } else {
+// 	fwrite(STDOUT, "{$studentName}, you should probably study more.\n");
+// }
