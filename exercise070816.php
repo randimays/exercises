d33563074c1f9e7b8e47503a1856052d08b4e30f
Randi Mays <randimays1024@gmail.com>
2016-07-08 10:42:24 -0500

    Adding debug exercise for Friday morning

---
+++ exercise070816.php
@@ -0,0 +1,57 @@
+<?php
+$friends = [
+	[
+		'name' => 'Oliver',
+		'books' => [
+			'Ansible for DevOps',
+			'Servers for hackers',
+		]
+	],
+	[
+		'name' => 'Barry',
+		'books' => [
+			'Working effectively with unit tests',
+			'50 quick ideas for your tests',
+		],
+	],
+	[
+		'name' => 'Jessica',
+		'books' => [
+			'Understanding the 4 rules of simple design',
+			'Principles of package design',
+		],
+	],
+	[
+		'name' => 'Clark',
+		'books' => [
+			'Selling test driven projects',
+		],
+	]
+];
+
+function searchByName($friends, $name) {
+	$filtered = [];
+	foreach ($friends as $friend) {
+		if (strpos($friend['name'], $name) !== false) {
+			$filtered[] = $friend;
+		}
+	}
+	return $filtered;
+}
+
+do {
+	echo '1. Show friends books', PHP_EOL;
+	echo '2. Search by name', PHP_EOL;
+	echo '3. Exit', PHP_EOL;
+	$option = fgets(STDIN);
+	switch ($option) {
+		case 1:
+			// TODO: Implement this one later...
+			break;
+		case 2:
+			echo 'Please enter a name: ';
+			$name = trim(fgets(STDIN));
+			var_dump(searchByName($friends, $name));
+			break;
+	}
+} while ($option != 3);
