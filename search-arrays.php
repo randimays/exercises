29bc5a5a1b80da5c312933594469959ca3994c12
Randi Mays <randimays1024@gmail.com>
2016-07-08 14:07:27 -0500

    First section of 6.5.2

--- search-arrays.php
+++ search-arrays.php
@@ -4,6 +4,14 @@ $names = ["Tina", "Dana", "Mike", "Amy", "Adam"];
 
 $compare = ["Tina", "Dean", "Mel", "Amy", "Michael"];
 
-$search = "Tina";
+$search = "Amy";
 
-var_dump(array_search($search, $names, true));
+function inArray ($search, $names) {
+	if (array_search($search, $names) === false) {
+		echo "FALSE\n";
+	} else {
+		echo "TRUE\n";
+	}
+}
+
+inArray($search, $names);
