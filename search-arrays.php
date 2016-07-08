7b785ef9a3d99a88539486bdf4a5e60a8f1bcd26
Randi Mays <randimays1024@gmail.com>
2016-07-08 14:27:42 -0500

    Second section of 6.5.2

--- search-arrays.php
+++ search-arrays.php
@@ -6,12 +6,32 @@ $compare = ["Tina", "Dean", "Mel", "Amy", "Michael"];
 
 $search = "Amy";
 
-function inArray ($search, $names) {
-	if (array_search($search, $names) === false) {
-		echo "FALSE\n";
-	} else {
-		echo "TRUE\n";
+// section 1
+
+// function inArray ($search, $names) {
+// 	if (array_search($search, $names) === false) {
+// 		echo "FALSE\n";
+// 	} else {
+// 		echo "TRUE\n";
+// 	}
+// }
+
+// inArray($search, $names);
+
+$animals = ["cat", "dog", "mouse", "gorilla", "giraffe", "elephant", "kangaroo", "duck"];
+
+$animals2 = ["mouse", "rat", "penguin", "gorilla", "dog", "koala"];
+
+
+function compareArrays ($array1, $array2) {
+	$commonValues = [];
+	foreach ($array1 as $key) {
+		if (array_search($key, $array2, true) !== false) {
+			$commonValues[] = $key;
+		}
 	}
+	return "The number of common values is " . count($commonValues) . ".\n";
 }
 
-inArray($search, $names);
+echo compareArrays($names, $compare);
+echo compareArrays($animals, $animals2);
