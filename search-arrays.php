89fcbc6d9d8fdccdc176e2b566f125b9614c6a30
Randi Mays <randimays1024@gmail.com>
2016-07-08 15:51:09 -0500

    Finishing 6.5.2

--- search-arrays.php
+++ search-arrays.php
@@ -1,37 +1,31 @@
 <?php
 
 $names = ["Tina", "Dana", "Mike", "Amy", "Adam"];
-
 $compare = ["Tina", "Dean", "Mel", "Amy", "Michael"];
 
-$search = "Amy";
-
-// section 1
-
-// function inArray ($search, $names) {
-// 	if (array_search($search, $names) === false) {
-// 		echo "FALSE\n";
-// 	} else {
-// 		echo "TRUE\n";
-// 	}
-// }
-
-// inArray($search, $names);
-
-$animals = ["cat", "dog", "mouse", "gorilla", "giraffe", "elephant", "kangaroo", "duck"];
-
-$animals2 = ["mouse", "rat", "penguin", "gorilla", "dog", "koala"];
-
-
-function compareArrays ($array1, $array2) {
-	$commonValues = [];
-	foreach ($array1 as $key) {
-		if (array_search($key, $array2, true) !== false) {
-			$commonValues[] = $key;
-		}
+function inArray ($search, $names) {
+	if (array_search($search, $names) === false) {
+		return false;
+	} else {
+		return true;
 	}
-	return "The number of common values is " . count($commonValues) . ".\n";
 }
 
-echo compareArrays($names, $compare);
-echo compareArrays($animals, $animals2);
+var_dump(inArray("Amy", $names));
+
+function compareArrays($array1, $array2) {
+	$numInCommon = 0;
+	foreach ($array1 as $item) {
+		if (array_search($item, $array2, true) !== false) {
+			$numInCommon++;
+		}
+	}
+	return $numInCommon;
+}
+
+var_dump(compareArrays($names, $compare));
+
+$animals = ["cat", "dog", "mouse", "gorilla", "giraffe", "elephant", "kangaroo", "duck"];
+$animals2 = ["mouse", "rat", "penguin", "gorilla", "dog", "koala"];
+
+var_dump(compareArrays($animals, $animals2));
