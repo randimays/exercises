a3d2bda7f612209d948161133f733fa5c9c35370
Randi Mays <randimays1024@gmail.com>
2016-07-11 11:48:52 -0500

    Adding updated exercise after explanation for 6.5.4

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,40 +1,19 @@
  <?php
 
 $alphaSort = false;
-
-if ($argc === 2) {
-	if ($argv[1] == "true") {
-		$alphaSort = $argv[1];
-	} elseif ($argv[1] == "false") {
-		$alphaSort = false;
-	} else {
-		fwrite(STDERR, "The second argument should be 'true' or 'false'." . PHP_EOL);
-		exit(1);
-	}
-}
-
-// Converts array into list n1, n2, ..., and n3
-function humanizedList($array) {
-	$humanizedArray = [];
-	foreach ($array as $key => $name) {
-		if ($key === (count($array)) - 1) {
-			array_push($humanizedArray, "and " . $name);
-		} else {
-			array_push($humanizedArray, $name);
-		}
-	}
-	return implode(", ", $humanizedArray);
-}
-
-// List of famous peeps
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+function humanizedList($physicistsArray, $alphaSort = false) {
+	if ($alphaSort) {
+		sort($physicistsArray);
+	}
+	$last = array_pop($physicistsArray);
+	array_push($physicistsArray, "and $last");
+	return implode(", ", $physicistsArray);
+}
 
 // TODO: Convert the string into an array
 $physicistsArray = explode(", ", $physicistsString);
-
-if ($alphaSort) {
-	sort($physicistsArray);
-}
 
 // Humanize that list
 $famousFakePhysicists = humanizedList($physicistsArray);
