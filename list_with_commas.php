458c4302e17ba5fa317bfbc5d283bf165e8b9952
Randi Mays <randimays1024@gmail.com>
2016-07-11 10:28:46 -0500

    Adding first section of 6.5.4

---
+++ list_with_commas.php
@@ -0,0 +1,28 @@
+ <?php
+
+// Converts array into list n1, n2, ..., and n3
+function humanizedList($array) {
+	$humanizedArray = [];
+	foreach ($array as $key => $name) {
+		if ($key === (count($array)) - 1) {
+			array_push($humanizedArray, "and " . $name);
+		} else {
+			array_push($humanizedArray, $name);
+		}
+	}
+	return implode(", ", $humanizedArray);
+}
+
+// List of famous peeps
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+// TODO: Convert the string into an array
+$physicistsArray = explode(", ", $physicistsString);
+
+// Humanize that list
+$famousFakePhysicists = humanizedList($physicistsArray);
+
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
+ ?>
