39e167073701df25a427f4703de124e210e4c0a0
Randi Mays <randimays1024@gmail.com>
2016-07-12 11:27:42 -0500

    Adding morning exercise

---
+++ exercise071216.php
@@ -0,0 +1,36 @@
+<?php 
+
+// Write a function to put the following array in alphabetical order without using any of the predefined sorting functions.
+
+$letters = ["e", "a", "g", "c", "i", "d", "f", "b", "h"];
+
+function alphabetical($array) {
+	$count = (count($array)-1);
+	for ($i = 0; $i < $count; $i++) {
+		for ($j = $i + 1; $j < $count; $j++) {
+			if ($array[$i] > $array[$j]) {
+				$temp = $array[$i];
+				$array[$i] = $array[$j];
+				$array[$j] = $temp;
+			}
+		}
+	}
+	return $array;
+}
+
+print_r(alphabetical($letters));
+
+
+// alternative solution
+
+$unsorted = $letters;
+$sorted = [];
+
+foreach ($letters as $index => $letter) {
+	$min = min($unsorted);
+	$sorted[] = $min;
+	$index = array_search($min, $unsorted);
+	unset($unsorted[$index]);
+}
+
+print_r($sorted);
