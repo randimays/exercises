ddd3b147b6219a64339def52d96dd179c96e969e
Randi Mays <randimays1024@gmail.com>
2016-07-13 14:05:49 -0500

    Attempting bonus on contact exercise

--- contacts-manager.php
+++ contacts-manager.php
@@ -59,6 +59,7 @@ function routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filena
 function viewContacts($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename) {
 	$formattedContacts = formatContactsArray($contactsArray);
 	print_r($formattedContacts);
+	// printResults($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
 	routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
 }
 
@@ -109,6 +110,46 @@ function printResults($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $fil
 	}
 		routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
 }
+
+// function printResults($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename) {
+// 	$result = searchContact($contactsArray, $searchMsg);
+// 	$formattedContacts = formatContactsArray($contactsArray);
+// 	$contactsTable = [];
+// 	$names = [];
+// 	$numbers = [];
+// 	if (resultCheck($result, $contactsArray, $searchMsg)) {
+// 		foreach ($formattedContacts as $contact) {
+// 			$longestName = max($contact);
+// 			$length = strlen($longestName);
+// 			foreach ($contact as $individual) {
+// 				$names[] = $contact["name"];
+// 				$numbers[] = $contact["number"];
+// 			}
+// 		}
+
+// 		foreach ($numbers as $number) {
+// 			str_pad($number, 13, " ", STR_PAD_RIGHT);
+// 			$contactsTable = [
+// 				"number" => $number
+// 			];
+// 		}
+
+// 		foreach ($names as $name) {
+// 			str_pad($name, $length, " ", STR_PAD_RIGHT);
+// 			$contactsTable = [
+// 				"name" => $name
+// 			];
+// 		}
+// 		var_dump($contactsTable);
+// 		print_r($formattedContacts[$result]);
+// 	}
+// 		routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
+
+// 		$contactsTable[] = $contactRow;
+// 	}
+// 	return $contactsTable;
+// 	}
+// }
 
 function resultCheck($result, $contactsArray, $searchMsg) {
 	if (!(is_numeric($result))) {
