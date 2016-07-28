ff23cec250f3dd14307a51692aa7b30ffa0d1f04
Randi Mays <randimays1024@gmail.com>
2016-07-28 13:32:58 -0500

    Updating 6.3.4 with exercise descriptions

--- break_continue.php
+++ break_continue.php
@@ -1,5 +1,7 @@
 <?php
 
+// Create a for loop that shows all even numbers between 1 and 100 using continue. Create another that counts from 1 to 100 but stops after 10 using break.
+
 foreach (range(1, 100) as $i) {
 	if ($i % 2 === 1) {
 		continue;
