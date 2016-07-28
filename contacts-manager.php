a59168d7f52699c9412e79aac509e3525af983f4
Randi Mays <randimays1024@gmail.com>
2016-07-28 13:46:42 -0500

    Updating 6.6.3 with exercise descriptions

--- contacts-manager.php
+++ contacts-manager.php
@@ -1,146 +1,164 @@
 <?php
 
-$filename = "contacts.txt";
-$mainMenuMsg = "Menu:\n1. View contacts.\n2. Add a new contact.\n3. Search a contact by name.\n4. Delete an existing contact.\n5. Exit.\nEnter an option (1, 2, 3, 4 or 5): ";
-$contactsArray = createContentsArray($filename);
-$searchMsg = "Enter first name of contact:\n";
+// Create a command line application that can: 1) show all your contacts 2) add a new contact 3) search a contact by his/her name and 4) delete an existing contact. Use contacts.txt to harvest this information and display it to the user.
+
 $deleteMsg = "Confirm this is the entry to delete with 'y'. Type 'n' to enter a different name.\n";
 
-function createContentsArray($filename) {
-	$handle = fopen($filename, 'r');
-	$contents = trim(fread($handle, filesize($filename)));
-	fclose($handle);
-	$contactsArray = explode("\n", $contents);
-	return $contactsArray;
+function initialMenu() {
+	fwrite(STDOUT, "Main Menu\n");
+	fwrite(STDOUT, "1. View contacts.\n");
+	fwrite(STDOUT, "2. Add a new contact.\n");
+	fwrite(STDOUT, "3. Search for a contact by name.\n");
+	fwrite(STDOUT, "4. Delete an existing contact.\n");
+	fwrite(STDOUT, "5. Exit.\n");
+	fwrite(STDOUT, "Enter an option (1, 2, 3, 4 or 5): \n");
+
+	$menuChoice = trim(fgets(STDIN));
+	return $menuChoice;
 }
 
-function formatContactsArray($contactsArray) {
-	foreach ($contactsArray as $namePhoneString) {
+function loadContacts($userSearch = "") {
+	$contacts = [];
+	$longestName = 0;
+	$longestNumber = 0;
+	$contents = trim(file_get_contents("contacts.txt"));
+	$contentsArray = explode("\n", $contents);
+
+	foreach ($contentsArray as $namePhoneString) {
 		$contact = [];
 		$namePhoneArray = explode("|", $namePhoneString);
 		$contact["name"] = $namePhoneArray[0];
+
 		if ((strlen($namePhoneArray[1])) > 7) {
-			$contact["number"] = 
+			$contact["formattedNumber"] = 
 				substr($namePhoneArray[1], 0, 3) . "-" .
 				substr($namePhoneArray[1], 3, 3) . "-" . 
 				substr($namePhoneArray[1], 6);
 		} else {
-			$contact["number"] =
+			$contact["formattedNumber"] =
 				substr($namePhoneArray[1], 0, 3) . "-" .
 				substr($namePhoneArray[1], 3);
 		}
-		$formattedContacts[] = $contact;
-	}
-	return $formattedContacts;
-}
 
-function initialMenu($mainMenuMsg) {
-	fwrite(STDOUT, $mainMenuMsg);
-	$menuChoice = "";
-	$menuChoice = trim(fgets(STDIN));
-	return $menuChoice;
-}
-	
-function routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename) {
-	$menuChoice = initialMenu($mainMenuMsg);
-		if ($menuChoice == 1) {
-			viewContacts($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
-		} elseif ($menuChoice == 2) {
-			addContact($contactsArray, $mainMenuMsg, "Enter the contact's first and last name: \n", $searchMsg, $deleteMsg, $filename);
-		} elseif ($menuChoice == 3) {
-			printResults($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
-		} elseif ($menuChoice == 4) {
-			deleteContact($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
-		} else {
-			exit(0);
-		}
-}
-
-function viewContacts($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename) {
-	$formattedContacts = formatContactsArray($contactsArray);
-	print_r($formattedContacts);
-	// printResults($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
-	routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
-}
-
-function addContact($contactsArray, $mainMenuMsg, $message, $searchMsg, $deleteMsg, $filename) {
-	do {
-		$result = searchContact($contactsArray, "Enter the contact's first and last name.\n");
-		$moretoAdd = "";
-
-		do {
-			fwrite(STDOUT, "There's already a contact with that name.\n");
-			$result = searchContact($contactsArray, "Enter the contact's first and last name.\n");
-		} while (is_numeric($result));
-
-		fwrite(STDOUT, "Enter the contact's 7 or 10 digit phone number with no spaces (ex. 1234567890): \n");
-		$contactNumber = trim(fgets(STDIN));
-		$handle = fopen($filename, "a");
-		fwrite($handle, $contactName . "|" . $contactNumber . PHP_EOL);
-		fclose($handle);
-		$contactsArray[] = $contactName . "|" . $contactNumber;
-		fwrite(STDOUT, "Do you have more contacts to add? Type 'y' or 'n'.\n");
-		$moreToAdd = trim(fgets(STDIN));
-	} while($moreToAdd === "y");
-
-	fwrite(STDOUT, "Record created successfully.\n");
-	routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
-}
-
-function searchContact($contactsArray, $searchMsg) {
-	fwrite(STDOUT, $searchMsg);
-	$contactName = trim(fgets(STDIN));
-	$formattedContacts = formatContactsArray($contactsArray);
-	$result;
-	foreach ($formattedContacts as $entry => $contact) {
-		foreach ($contact as $info => $detail) {
-			if (strpos($detail, $contactName) !== false) {
-				$result = $entry;
-				return $result;
+		if ($userSearch === "" || strpos($contact["name"], $userSearch) !== false) {
+			if ($longestName < strlen($contact["name"])) {
+				$longestName = strlen($contact["name"]);
 			}
+			if ($longestNumber < strlen($contact["formattedNumber"])) {
+				$longestNumber = strlen($contact["formattedNumber"]);
+			}
+
+			$contacts[] = $contact;
 		}
 	}
+
+	$data = [];
+	$data["contacts"] = $contacts;
+	$data["longestName"] = $longestName + 2;
+	$data["longestNumber"] = $longestNumber + 2;
+
+	return $data;
 }
 
-function printResults($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename) {
-	$result = searchContact($contactsArray, $searchMsg);
-	if (resultCheck($result, $contactsArray, $searchMsg)) {
-		$formattedContacts = formatContactsArray($contactsArray);
-		print_r($formattedContacts[$result]);
+function displayContacts($contacts, $longestName, $longestNumber) {
+	fwrite(STDOUT, PHP_EOL);
+	fwrite(STDOUT, str_pad("Name", $longestName, " ", STR_PAD_RIGHT) . "|" . str_pad("Phone", $longestNumber, " ", STR_PAD_BOTH) . "|" . PHP_EOL);
+	fwrite(STDOUT, str_pad("", $longestName + $longestNumber + 2, "-") . PHP_EOL);
+
+	foreach ($contacts as $contact) {
+		fwrite(STDOUT, str_pad($contact["name"], $longestName, " ", STR_PAD_RIGHT) . "|" . str_pad($contact["formattedNumber"], $longestNumber, " ", STR_PAD_BOTH) . "|" . PHP_EOL);
 	}
-		routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
+
+	fwrite(STDOUT, PHP_EOL);
 }
 
-function resultCheck($result, $contactsArray, $searchMsg) {
-	if (!(is_numeric($result))) {
-		do {
-			fwrite(STDOUT, "Contact not found. Please search again.\n");
-			searchContact($contactsArray, $searchMsg);
-		} while (!(is_numeric($result)));
-	} else {
-		return true;
+function viewContacts() {
+	extract(loadContacts());
+	displayContacts($contacts, $longestName, $longestNumber);
+}
+
+function addContact() {
+	fwrite(STDOUT, "Enter the contact's first and last name.\n");
+	$name = trim(fgets(STDIN));
+	extract(loadContacts($name));
+
+	while ($name == "") {
+		fwrite(STDOUT, "Name must not be empty.\n");
+		fwrite(STDOUT, "Enter the contact's first and last name.\n");
+		$name = trim(fgets(STDIN));
 	}
-}
 
-function deleteContact($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename) {
-	do {
-		$result = searchContact($contactsArray, $searchMsg);
-		if (resultCheck($result, $contactsArray, $searchMsg)) {
-			$formattedContacts = formatContactsArray($contactsArray);
-			print_r($formattedContacts[$result]);
-			fwrite(STDOUT, $deleteMsg);
-			$userChoice = trim(fgets(STDIN));
-		}
-	} while ($userChoice === 'n');
-	
-	unset($contactsArray[$result]);
-	$newContactsString = implode("\n", $contactsArray);
+	fwrite(STDOUT, "Enter the contact's 7 or 10 digit phone number with no spaces (ex. 1234567890): \n");
+	$number = trim(fgets(STDIN));
 
-	$handle = fopen($filename, 'w');
-	fwrite($handle, $newContactsString);
+	while (!is_numeric($number) || (strlen($number) !== 7) && (strlen($number) !== 10)) {
+		fwrite(STDOUT, "Phone numbers can only contain numbers and must be 7 or 10 digits in length.\n");
+		fwrite(STDOUT, "Enter the contact's 7 or 10 digit phone number with no spaces (ex. 1234567890): \n");
+		$number = trim(fgets(STDIN));
+	}
+
+	$handle = fopen("contacts.txt", "a");
+	fwrite($handle, $name . "|" . $number . PHP_EOL);
 	fclose($handle);
-	fwrite(STDOUT, "Record deleted.\n");
-	routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
+	fwrite(STDOUT, "Record created successfully.\n");
 }
 
-routeUser($contactsArray, $mainMenuMsg, $searchMsg, $deleteMsg, $filename);
+function searchForContact() {
+	fwrite(STDOUT, "Enter the contact's first and last name.\n");
+	$name = trim(fgets(STDIN));
+	extract(loadContacts($name));
+	displayContacts($contacts, $longestName, $longestNumber);
+}
+
+function deleteContact() {
+	viewContacts();
+	$userChoice = "";
+	do {
+		fwrite(STDOUT, "Enter the contact's first and last name.\n");
+		$name = trim(fgets(STDIN));
+		searchRecords($name);
+		fwrite(STDOUT, "Are you sure you want to delete this contact? Type 'y' for yes, 'n' for no.\n");
+		$userChoice = trim(fgets(STDIN));
+	} while ($userChoice === 'n');
+
+	$name = $deleteName;
+	
+	extract(loadContacts());
+	$handle = fopen("contacts.txt", "w");
+
+	foreach ($contacts as $key => $contact) {
+		if ($contact["name"] !== $deleteName) {
+			fwrite($handle, $contact["name"] . "|" . $contact["number"] . PHP_EOL);
+		} else {
+			fwrite(STDOUT, "Record deleted.\n");
+		}
+	}		
+
+	fclose($handle);
+}
+
+do {
+	$menuChoice = intval(initialMenu());
+
+	switch ($menuChoice) {
+		case 1:
+			viewContacts();
+			break;
+		case 2:
+			addContact();
+			break;
+		case 3:
+			searchForContact();
+			break;
+		case 4:
+			deleteContact();
+			break;
+		case 5:
+			fwrite(STDOUT, "Exiting app.\n");
+			exit(0);
+			break;
+		default:
+			fwrite(STDOUT, "Invalid response.\n");
+			break;
+	}
+} while ($menuChoice !== "5");
